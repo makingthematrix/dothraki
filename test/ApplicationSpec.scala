294e58cda8e6b3774ec8473ff6941227d76122a5
Pawel Ptaszynski <pawel@ptaszynski.cc>
2015-02-12 20:59:00 +0100

    initial of translation service

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,8 +1,11 @@
 import models.Quote
 import org.specs2.mutable._
+import play.api.Logger
 
 import play.api.test._
 import play.api.test.Helpers._
+
+import play.api.libs.json.{JsString, JsObject, Json}
 
 /**
  * Test repositioning operations.
@@ -17,6 +20,32 @@ class ApplicationSpec extends Specification {
           "I will kill the men in iron suits",
           "She will speak before me; until then you will keep yourself quiet"
         ))
+      }
+  }
+
+  "The router" should {
+    "respond to GET translate action" in
+      new WithApplication(FakeApplication(additionalConfiguration = inMemoryDatabase("test"))) {
+        val Some(result) = route(FakeRequest(GET, "/translate/test"))
+
+        status(result) must equalTo(OK)
+      }
+
+    "respond to POST translate action" in
+      new WithApplication(FakeApplication(additionalConfiguration = inMemoryDatabase("test"))) {
+        val request = FakeRequest(POST, "/translate").withJsonBody(JsObject(Seq(
+          "word" -> JsString("test")))).withHeaders((CONTENT_TYPE,"application/json"))
+        //Logger.debug(request.headers.toString())
+        val Some(result) = route(FakeRequest(POST, "/translate"))
+
+        status(result) must equalTo(OK)
+      }
+
+    "respond to index action" in
+      new WithApplication(FakeApplication(additionalConfiguration = inMemoryDatabase("test"))) {
+        val Some(result) = route(FakeRequest(GET, "/"))
+
+        status(result) must equalTo(OK)
       }
   }
 
